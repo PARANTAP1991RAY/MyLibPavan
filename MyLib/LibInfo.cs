5827d454e2388ba7fb073bf3acac1dc7c4ed83f3
parantap <parantap.ray@gmail.com>
2020-12-28 17:52:34 +0530

    Add project files.

---
+++ MyLib/LibInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib
+{
+   public class LibInfo
+    {
+        public string VersionInfo
+        {
+            get { return "Initial Library Info"; }// Test
+        }
+    }
+}
